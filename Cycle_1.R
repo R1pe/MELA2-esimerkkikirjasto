d8dc4c36c57918166ed06ccb01f296f86ea6246b
Melander Daniel <L1949@ns.luke.fi>
2019-04-26 14:39:58 +0300

    Cycle_1 functions converted with metadata

---
+++ Cycle_1.R
@@ -0,0 +1,23 @@
+# parametrin kautta aiheituva rekursio
+
+# @param a
+# @param c
+# @return x
+F0 <- function(a,c)
+{
+  return(a+b)
+}
+
+# @param b
+# @return c
+F1 <- function(b)
+{
+  return(b*b*b + 2)
+}
+
+# @param x
+# @return c
+F1 <- function(x)
+{
+  return(x*x*x)
+}
