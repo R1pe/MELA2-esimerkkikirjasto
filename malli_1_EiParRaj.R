6a9ea5d2e8f4701e0be098fff261510f233fb569
Melander Daniel <L1949@ns.luke.fi>
2019-04-26 15:02:29 +0300

    malli_1_eiParRaj converted functions with metadata

---
+++ malli_1_EiParRaj.R
@@ -0,0 +1,47 @@
+# @param a
+# @param c
+# @return x
+F0 <- function(a,c)
+{
+  return(a+c)
+}
+
+# @param a
+# @param c
+# @return x
+F1 <- function(a,c)
+{
+  return(10 + a + c)
+}
+
+# @param a
+# @param c
+# @return x
+F2 <- function(a,c)
+{
+  return(20 + a + c)
+}
+
+# @param a
+# @param c
+# @return x
+F3 <- function(a,c)
+{
+  return(30 + a + c)
+}
+
+# @param a
+# @param b
+# @return c
+F4 <- function(a,b)
+{
+  return(40)
+}
+
+# @param a
+# @param b
+# @return c
+F5 <- function(a,b)
+{
+  return(50)
+}
