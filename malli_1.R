0837263b3c45defea97c6246c6313c04a9abeec1
Melander Daniel <L1949@ns.luke.fi>
2019-04-29 08:38:40 +0300

    Malli_1 conversion with metadata

---
+++ malli_1.R
@@ -0,0 +1,22 @@
+# @param a
+# @param c
+# @return x
+F0 <- function(a,c)
+{
+    return(a+c)
+}
+
+# @param b
+# @return c
+F0 <- function(b)
+{
+    return(b*b*b+2)
+}
+
+# @param a
+# @return c
+F0 <- function(a)
+{
+    return(a*a*a)
+}
+
